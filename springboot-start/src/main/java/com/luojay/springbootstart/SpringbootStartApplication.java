66e7830030a725d00abdd6e97c1aa2f784139052
me <codeluojay@qq.com>
2019-12-17 23:43:01 +0800

    init

---
+++ springboot-start/src/main/java/com/luojay/springbootstart/SpringbootStartApplication.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStartApplication.class, args);
+    }
+
+}
