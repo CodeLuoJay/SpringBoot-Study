d1fa69f54d9435027812f2622e24c45446e2fdf1
me <codeluojay@qq.com>
2019-12-19 23:07:35 +0800

    SpringBootBanner

---
+++ springboot-banner/src/main/java/com/luojay/springbootbanner/SpringbootBannerApplication.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootbanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBannerApplication.class, args);
+    }
+
+}
---
+++ springboot-banner/src/test/java/com/luojay/springbootbanner/SpringbootBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootbanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
