4e1a3cbf0b0d26455a4cafe9535c4a5e686908c3
me <codeluojay@qq.com>
2019-12-24 23:41:43 +0800

    add SpringBoot Themeleaf

---
+++ springboot-thymeleaf/src/main/java/com/luojay/springbootthymeleaf/controller/ThymeleafController.java
@@ -0,0 +1,12 @@
+package com.luojay.springbootthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ThymeleafController {
+    @RequestMapping("/getThymeleaf")
+    public String Welcome(){
+        return "luojay";
+    }
+}
