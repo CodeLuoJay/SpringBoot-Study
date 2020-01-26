505128aace357f070239887fbdb97b37bdf349ce
me <codeluojay@qq.com>
2020-01-26 22:48:52 +0800

    添加SpringBoot整合Thymeleaf和LayUI制作简单登录页面

---
+++ springboot-themeleaf-layui/src/main/java/com/luojay/springbootthemeleaflayui/SpringbootThemeleafLayuiApplication.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootthemeleaflayui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThemeleafLayuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThemeleafLayuiApplication.class, args);
+    }
+
+}
