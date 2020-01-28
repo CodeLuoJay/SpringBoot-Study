e6e81b18c5803047aff68f8da1c4fc9b3c2b5e9a
me <codeluojay@qq.com>
2020-01-28 11:24:28 +0800

    添加SpringBoot整合Thymeleaf和Bootstrap制作简单登录页面

---
+++ springboot-thymeleaf-bootstrapui/src/main/java/com/luojay/springbootthymeleafbootstrapui/SpringbootThymeleafBootstrapuiApplication.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootthymeleafbootstrapui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafBootstrapuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafBootstrapuiApplication.class, args);
+    }
+
+}
---
+++ springboot-thymeleaf-bootstrapui/src/test/java/com/luojay/springbootthymeleafbootstrapui/SpringbootThymeleafBootstrapuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luojay.springbootthymeleafbootstrapui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafBootstrapuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
