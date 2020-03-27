bf63273c4f088b026d77c7e9713be7e7371fa77e
me <codeluojay@qq.com>
2020-03-27 14:56:17 +0800

    update

--- springboot-start/src/main/java/com/luojay/springbootstart/controller/SpringBootController.java
+++ springboot-start/src/main/java/com/luojay/springbootstart/controller/SpringBootController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class SpringBootController {
-    @ResponseBody
     @RequestMapping("/hello")
     public String SayHello(){
         return "Hello SpringBoot!";
