edcbb89fbe442fc20bf5e1d760660df8849ab555
me <codeluojay@qq.com>
2020-03-28 12:16:11 +0800

    add druid dataSource

---
+++ springboot-druid/src/test/java/com/luojay/druid/SpringbootDruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luojay.druid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
